c5b44ff502659b2f9c9e487b2e29df8ecc3692f3
Spolavore <75754941+Spolavore@users.noreply.github.com>
2024-01-20 19:07:24 -0300

    aplicando contato inferior nas caixas
    suavizando a animação de pulo e impedindo que o jogador pule enquanto caia

--- TrabalhoFinal-FCG/include/collisions.h
+++ TrabalhoFinal-FCG/include/collisions.h
@@ -6,6 +6,6 @@
 
 void verifyCratesCollisions(bool user_can_move[], glm::vec4 camera_c_position,
                             glm::vec4 camera_view_vector, std::map<std::string, SceneObject>& g_VirtualScene,
-                            std::vector<glm::vec3> crates_translation_models);
-void verifyFalling(glm::vec4* camera_c_position, float delta_t,bool *usuario_esta_caindo,  bool *pode_pular);
+                            std::vector<glm::vec3> crates_translation_models, bool *pode_pular);
+void verifyFalling(glm::vec4* camera_c_position, float delta_t,bool *usuario_esta_caindo,  bool *pode_pular, bool *usuario_esta_pulando);
 #endif // COLLISIONS_H_INCLUDED
